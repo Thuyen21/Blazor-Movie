ae2e2cea68c4f1a36b49e9cf0845f745ee8ff5ce
Thuyen21 <65522631+Thuyen21@users.noreply.github.com>
2021-08-13 21:16:08 +0700

    Maui

---
+++ MauiApp1/MauiApp1/MauiApp1/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1
+{
+    public class CustomAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+        }
+    }
+}
