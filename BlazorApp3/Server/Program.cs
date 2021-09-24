0b8efe28a9c445d450fa153d2be9d93b120329ec
Thuyen21 <65522631+Thuyen21@users.noreply.github.com>
2021-09-24 22:37:36 +0700

    Update Program.cs

--- BlazorApp3/Server/Program.cs
+++ BlazorApp3/Server/Program.cs
@@ -53,13 +53,11 @@ WebApplication? app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseWebAssemblyDebugging();
-    app.UseForwardedHeaders();
 }
 else
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseForwardedHeaders();
     app.UseHsts();
 }
 
