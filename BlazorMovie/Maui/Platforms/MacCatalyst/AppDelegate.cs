42b4cfd6f58009105f263c529dd2f78851d33063
Thuyen21 <65522631+Thuyen21@users.noreply.github.com>
2022-02-13 17:22:03 +0700

    up

--- BlazorMovie/Maui/Platforms/MacCatalyst/AppDelegate.cs
+++ BlazorMovie/Maui/Platforms/MacCatalyst/AppDelegate.cs
@@ -5,5 +5,5 @@ namespace Maui;
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
 {
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
