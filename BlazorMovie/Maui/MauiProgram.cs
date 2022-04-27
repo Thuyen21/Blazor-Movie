c88d6e50f27b0bd9af7096a862e0378a101ae311
Thuyen21 <65522631+Thuyen21@users.noreply.github.com>
2022-04-27 23:54:46 +0700

    Maui 6.0.300 rc.2 (#179)
    * up
    
    * up
    
    * Update BlazorMovie.Maui.csproj
    
    * up
    
    * up
    
    * Update dotnet.yml
    
    * Update dotnet.yml
    
    * Update dotnet.yml
    
    * Update dotnet.yml
    
    * up
    
    * Update dotnet.yml
    
    * Update dotnet.yml
    
    * Update dotnet.yml
    
    * up
    
    * Update dotnet.yml
    
    * Update dotnet.yml
    
    * Update dotnet.yml
    
    * Update dotnet.yml
    
    * Update dotnet.yml
    
    * up
    
    * Update global.json
    
    * Revert "Update global.json"
    
    This reverts commit 59621ff2059d3d6bbaedd5fe6bd1dd3553d1f80b.
    
    * Update global.json
    
    * up
    
    * Update global.json
    
    * Update global.json
    
    * Update README.md
    
    * up
    
    * up

--- BlazorMovie/Maui/MauiProgram.cs
+++ BlazorMovie/Maui/MauiProgram.cs
@@ -12,23 +12,26 @@ public static class MauiProgram
     public static MauiApp CreateMauiApp()
     {
         MauiAppBuilder builder = MauiApp.CreateBuilder();
-        _ = builder
-            .RegisterBlazorMauiWebView()
+        builder
             .UseMauiApp<App>()
             .ConfigureFonts(fonts =>
             {
-                _ = fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
             });
-        _ = builder.Logging.ClearProviders();
-        _ = builder.Logging.AddConsole();
-        _ = builder.Services.AddBlazorWebView();
-        _ = builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("https://movie213.herokuapp.com/") });
-        _ = builder.Services.AddOptions();
-        _ = builder.Services.AddAuthorizationCore();
-        _ = builder.Services.AddSingleton<AccountService>();
-        _ = builder.Services.AddSingleton<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
-        _ = builder.Services.AddMudServices();
-        _ = builder.Services.AddSingleton<ShowAlertService>();
+        builder.Services.AddMauiBlazorWebView();
+#if DEBUG
+		builder.Services.AddBlazorWebViewDeveloperTools(); 
+#endif
+        builder.Logging.ClearProviders();
+        builder.Logging.AddConsole();
+        builder.Services.AddBlazorWebView();
+        builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("https://movie213.herokuapp.com/") });
+        builder.Services.AddOptions();
+        builder.Services.AddAuthorizationCore();
+        builder.Services.AddSingleton<AccountService>();
+        builder.Services.AddSingleton<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+        builder.Services.AddMudServices();
+        builder.Services.AddSingleton<ShowAlertService>();
 
         return builder.Build();
     }
