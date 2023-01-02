57a306c1ab3e93fc0d09c29cbca0fdfb374b10e3
Thuyen21 <65522631+Thuyen21@users.noreply.github.com>
2023-01-02 23:47:33 +0700

    doing repository

---
+++ BlazorMovie.Entities/Context/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Azure;
+using Microsoft.Extensions.Hosting;
+using BlazorMovie.Entities.Entities;
+
+namespace BlazorMovie.Entities.Context;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ BlazorMovie.Repositories/Base/BaseRepository.cs
@@ -0,0 +1,74 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using BlazorMovie.Entities.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorMovie.Repositories.Base;
+
+public class BaseRepository<TEntity> where TEntity : class
+{
+    protected readonly ApplicationDbContext context;
+    protected readonly DbSet<TEntity> dbSet;
+    protected readonly IMapper mapper;
+
+    public BaseRepository(ApplicationDbContext context, IMapper mapper)
+    {
+        this.context = context;
+        dbSet = context.Set<TEntity>();
+        this.mapper = mapper;
+    }
+    public virtual async Task<IEnumerable<TEntity>> GetAsync(int? page = null, int? pageSize = null,
+        Expression<Func<TEntity, bool>>? filter = null,
+        Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null)
+    {
+        IQueryable<TEntity> query = dbSet.AsQueryable();
+        if (filter != null)
+        {
+            query = query.Where(filter);
+        }
+        if (orderBy != null)
+        {
+            query = orderBy(query);
+        }
+        if (page != null)
+        {
+            query = pageSize != null
+                ? query.Skip((page.Value - 1) * pageSize.Value).Take(pageSize.Value)
+                : query.Skip((page.Value - 1) * 10).Take(10);
+        }
+        return await query.ToListAsync();
+    }
+    public virtual async Task<IEnumerable<TResult>> GetAsync<TResult>(int? page = null, 
+        int? pageSize = null, Expression<Func<TEntity, bool>>? filter = null, 
+        Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null)
+    {
+        IQueryable<TEntity> query = dbSet.AsQueryable();
+
+        if (filter != null)
+        {
+            query = query.Where(filter);
+        }
+        if (orderBy != null)
+        {
+            query = orderBy(query);
+        }
+
+        if (page != null)
+        {
+            query = pageSize != null
+                ? query.Skip((page.Value - 1) * pageSize.Value).Take(pageSize.Value)
+                : query.Skip((page.Value - 1) * 10).Take(10);
+        }
+        return await query.ProjectTo<TResult>(mapper.ConfigurationProvider).ToListAsync();
+    }
+    public async Task<bool> AnyAsync(Expression<Func<TEntity, bool>> filter)
+    {
+        return await dbSet.AnyAsync(filter);
+    }
+}
